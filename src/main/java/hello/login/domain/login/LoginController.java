f6143e2dafc5f9afbb630bb77cf1f1c62122c1ed
Kewon <48837736+kewon96@users.noreply.github.com>
2022-11-24 00:25:18 +0900

    login/logout use cookie

--- src/main/java/hello/login/domain/login/LoginController.java
+++ src/main/java/hello/login/domain/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -24,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if(bindingResult.hasErrors()) return "login/loginForm";
 
         Member login = service.login(form.getLoginId(), form.getPassword());
@@ -34,10 +37,38 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
 
+        // Cookie에 시간정보를 안주면 Session Cookie
+
+        for(int i = 0; i < 5; i++) {
+            Cookie cookie = new Cookie("memberId", String.valueOf(i));
+            response.addCookie(cookie);
+        }
+
+//        Cookie cookie = new Cookie("memberId", String.valueOf(login.getId()));
+//        response.addCookie(cookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
+    }
+
+    /**
+     * cookie 만료시키기
+     * @param response
+     * @param cookieName
+     */
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 종료 날짜를 0으로 설정
+
+        response.addCookie(cookie);
     }
 
 }
