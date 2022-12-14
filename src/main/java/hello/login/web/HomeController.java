db5ab196ff42fb284ef8d4233f27b45e9ef4dc9f
Kewon <48837736+kewon96@users.noreply.github.com>
2022-12-15 00:15:01 +0900

    @SessionAttribute TrackingModes

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -16,27 +17,13 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final SessionManager sessionManager;
-
     /**
      * 메인URL에 들어갔을 때 어떤 URL로 이동시킬지 결정하는 controller
-     * <p>session에 따라 결정되며 이때 session이 없는경우 session을 생성하지 않는다.</p>
-     * @param request
+     * @param loginMember session에 담아있는 Member객체 / @SessionAttribute로 인해 session이 새로 생성될 일은 없다.
      * @param model
-     * @return
      */
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model)  {
-        // 이때 session을 생성할 의도가 없기 때문에 false로 설정
-        // 결국 session도 메모리를 사용하기 때문에 쓸데없이 쓸일이 없는게 좋다
-        HttpSession session = request.getSession(false);
-
-        if(session == null) return "home";
-
-        // sessionManager에 저장된 회원정보 조회
-        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
-
-        // session에 회원 데이터가 없으면...
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model)  {
         if(loginMember == null) return "home";
 
         model.addAttribute("member", loginMember);
