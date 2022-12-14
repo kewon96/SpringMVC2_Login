a7711c1e54125abe9a50e7acef1056b73aa61bd1
Kewon <48837736+kewon96@users.noreply.github.com>
2022-12-15 00:02:57 +0900

    apply HttoSession

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -17,15 +18,28 @@ public class HomeController {
 
     private final SessionManager sessionManager;
 
+    /**
+     * 메인URL에 들어갔을 때 어떤 URL로 이동시킬지 결정하는 controller
+     * <p>session에 따라 결정되며 이때 session이 없는경우 session을 생성하지 않는다.</p>
+     * @param request
+     * @param model
+     * @return
+     */
     @GetMapping("/")
     public String homeLogin(HttpServletRequest request, Model model)  {
+        // 이때 session을 생성할 의도가 없기 때문에 false로 설정
+        // 결국 session도 메모리를 사용하기 때문에 쓸데없이 쓸일이 없는게 좋다
+        HttpSession session = request.getSession(false);
+
+        if(session == null) return "home";
 
         // sessionManager에 저장된 회원정보 조회
-        Member member = (Member) sessionManager.getSession(request);
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
-        if(member == null) return "home";
+        // session에 회원 데이터가 없으면...
+        if(loginMember == null) return "home";
 
-        model.addAttribute("member", member);
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -29,27 +31,33 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if(bindingResult.hasErrors()) return "login/loginForm";
 
-        Member login = service.login(form.getLoginId(), form.getPassword());
+        Member loginMember = service.login(form.getLoginId(), form.getPassword());
 
-        if(login == null) {
+        if(loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
 
         // 로그인 성공 처리
+        // session이 있으면 session반환, 없으면 session 새로 생성
+        // 단, 무조건 새로 생성하고싶다면 parameter에 true를 넣어주면 된다.(는 기본값)
+        HttpSession session = request.getSession();
 
-        // sessionManager를 통해 session을 생성하고 회원데이터 보관
-        sessionManager.createSession(login, response);
+        // session에 로그인 회원정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
         return "redirect:/";
     }
 
     @PostMapping("/logout")
     public String logout(HttpServletRequest request) {
-        sessionManager.expireSession(request);
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
 
         return "redirect:/";
     }
